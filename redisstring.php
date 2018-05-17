46ff6ed228b6c8fe65004eee83f9f1a3887f4fea
dhywjx <2787811826@qq.com>
2018-05-17 11:14:17 +0800

    学习2

--- redisstring.php
+++ redisstring.php
@@ -6,14 +6,14 @@
  *Time:11:11
  */
 $redis = new \Redis();
-$redis->connect("127.0.0.1", 7200);
+$redis->connect("139.224.117.254", 7200);
 
-$redis->delete("string1");
-$redis->set("string1", "val1");
-$val = $redis->get("string1");
+$redis->delete("test");
+$redis->set("test", "wangjx");
+$val = $redis->get("test");
 var_dump($val);
-
-$redis->set("string1", 4);
-$redis->incr("string1", 2);
-$val = $redis->get("string1");
-var_dump($val);
+//
+//$redis->set("string1", 4);
+//$redis->incr("string1", 2);
+//$val = $redis->get("string1");
+//var_dump($val);
