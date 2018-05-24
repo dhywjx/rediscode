a9c134757cc8be0cdc620fbdc7353b32ab417c25
dhywjx <2787811826@qq.com>
2018-05-24 10:46:52 +0800

    修改

--- redisstring.php
+++ redisstring.php
@@ -5,7 +5,7 @@
  *Date:2018/4/30
  *Time:11:11
  */
-$redis = new Redis();
+$redis = new \Redis();
 $redis->connect("139.224.117.254", 7200);
 $redis->delete("test");
 $redis->set("test", "wangjx");
